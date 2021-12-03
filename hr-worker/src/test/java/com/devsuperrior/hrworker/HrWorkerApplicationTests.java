e326c46cfdc42237cbe0e0cbfe75f13603540802
unknown <fabio.borges@gmail.com>
2021-12-02 21:54:45 -0300

    Project hr-worked created

---
+++ hr-worker/src/test/java/com/devsuperrior/hrworker/HrWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperrior.hrworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
