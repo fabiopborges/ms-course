00631c2d4d33b9228b95a13d2668ee08451eb3b4
unknown <fabio.borges@gmail.com>
2021-12-06 22:20:44 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/devsuperrior/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/devsuperrior/hrpayroll/services/PaymentService.java
@@ -1,15 +1,33 @@
 package com.devsuperrior.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.devsuperrior.hrpayroll.entities.Payment;
+import com.devsuperrior.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Fabio", 200.0, days);
+
+		Map<String, String> uriVaribles = new HashMap<>();
+		uriVaribles.put("id", String.valueOf(workerId));
+
+		Worker worker = restTemplate.getForObject(workerHost.concat("/workers/{id}/"), Worker.class, uriVaribles);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+
 	}
-	
-	
+
 }
