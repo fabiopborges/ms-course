d7e72b7757670ee3a5b5074b54f9c5db98498cf1
unknown <fabio.borges@gmail.com>
2021-12-07 10:58:20 -0300

    Project hr-api-gatway-zuul created

---
+++ hr-api-gatway-zuul/src/test/java/com/devsuperrior/hrapigatwayzuul/HrApiGatwayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperrior.hrapigatwayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatwayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
