a288488353af62b99b66c21827ce5218ce54f163
unknown <fabio.borges@gmail.com>
2021-12-07 22:20:38 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/devsuperrior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperrior/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,12 +20,21 @@ import com.devsuperrior.hrworker.repositories.WorkerRepository;
 public class WorkerResource {
 
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
-	
+
+	@Value("${test.config}")
+	private String testConfig;
+
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private WorkerRepository repository;
+
+	@RequestMapping(value="/configs")
+	public ResponseEntity<Void> getConfig() {
+		logger.info("CONFIG = " + testConfig);
+		return ResponseEntity.noContent().build();
+	}
 
 	@RequestMapping
 	public ResponseEntity<List<Worker>> findAll() {
@@ -34,15 +44,12 @@ public class WorkerResource {
 
 	@RequestMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) {
-		
+
 		/*
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}*/
-		
+		 * try { Thread.sleep(3000); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 */
+
 		logger.info("PORT = " + env.getProperty("local.server.port"));
 		Worker worker = repository.findById(id).get();
 		return ResponseEntity.ok(worker);
